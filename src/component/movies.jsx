1dd17ab45cb14316dcc8ae969f3192b9324845f1
Amr-okasha <78168092+Amr-okasha@users.noreply.github.com>
2021-05-01 15:00:54 +0200

    create-movies-component-ui

---
+++ src/component/movies.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movies: getMovies(),
+    };
+  }
+
+  deleteHandler = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({
+      movies,
+    });
+  };
+
+  render() {
+    const { length: count } = this.state.movies;
+
+    return count === 0 ? (
+      <React.Fragment>
+        <p>There are not movies in the data base </p>
+        <p>Total Number :<span className={this.handleCountstyles()}>{this.handleCountResult()}</span> </p>
+      </React.Fragment>
+    ) : (
+      <React.Fragment>
+        
+        <p>
+          Showing <span className={this.handleCountstyles()}>{count}</span> movies in
+          the database
+        </p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Tittle</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => {
+              return (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <button
+                      onClick={() => this.deleteHandler(movie)}
+                      className="btn btn-danger btn-sm"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+  handleCountResult() {
+    const { length: count } = this.state.movies;
+
+    return count === 0 ? "Zero" : count;
+  }
+  handleCountstyles(){
+    const { length: count } = this.state.movies;
+      let badge = "badge bg-"
+    return badge += count ===0? "warning" : "primary" 
+  }
+}
+
+export default Movies;
